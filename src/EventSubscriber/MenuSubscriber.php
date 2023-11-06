58fb56ad13be6412e7ab949279f265e7e01fd249
Florian Bouché <florian.bouche@onisep.fr>
2023-11-06 15:30:27 +0100

    Ibexa 4 support (4)

--- src/EventSubscriber/MenuSubscriber.php
+++ src/EventSubscriber/MenuSubscriber.php
@@ -6,17 +6,14 @@ use eZ\Publish\API\Repository\PermissionResolver;
 use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
 use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MenuSubscriber implements EventSubscriberInterface
 {
     private PermissionResolver $permissionResolver;
-    private TranslatorInterface $translator;
 
-    public function __construct(PermissionResolver $permissionResolver, TranslatorInterface $translator)
+    public function __construct(PermissionResolver $permissionResolver)
     {
         $this->permissionResolver = $permissionResolver;
-        $this->translator = $translator;
     }
 
     public static function getSubscribedEvents(): array
@@ -33,8 +30,9 @@ class MenuSubscriber implements EventSubscriberInterface
 
         if ($this->permissionResolver->hasAccess('ibexa_custom_settings', 'key_edit')) {
             $menu[MainMenuBuilder::ITEM_ADMIN]->addChild('ibexa_custom_settings', [
-                'label' => $this->translator->trans('ibexa_custom_settings.menu.label'),
+                'label' => 'ibexa_custom_settings.menu.label',
                 'route' => 'ibexa_custom_settings_index',
+                'extras' => ['translation_domain' => 'messages']
             ]);
         }
     }
